cd6388ab213249828dc5eaeabc6a0a090e8f92ea
Cyril Bellec <cbellec@normandiewebschool.fr>
2023-12-07 12:06:16 +0100

    Ajoutez des fichiers projet.

---
+++ SiteOnePage/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SiteOnePage.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
